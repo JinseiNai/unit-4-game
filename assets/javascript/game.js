7f7d4ce942643f15952d50c5b47aabd39f6ca392
Calvin <jinseinai@gmail.com>
2018-06-09 01:33:06 -0700

    added sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,6 +24,7 @@ let game = {
         this.rupees.value = [];
         for (i = 0; i < 4; i++) {
             let randomValue = Math.floor(Math.random() * 12) + 1;
+            // No duplicate rupee values
             while (this.rupees.value.indexOf(randomValue) !== -1) {
                 randomValue = Math.floor(Math.random() * 12) + 1;
             }
@@ -37,31 +38,39 @@ let game = {
     },
 }
 
+// On browser load
 $(document).ready(function() {
     startGame();
     updateStatus();
 });
 
+// Setting game and resetting values
 function startGame() {
     game.generateScore();
     game.generateRupeeValue();
     game.yourScore = 0;
 }
 
+// Check score
 function scoreChecker() {
     if (game.yourScore === game.randomScore) {
+        let sound = document.getElementById('winSound');
+        sound.play();
         game.wins++;
         alert('You Won!');
         startGame();
     }
 
     if (game.yourScore > game.randomScore) {
+        let sound = document.getElementById('lostSound');
+        sound.play();
         game.losses++;
         alert('...Sorry, you lost...');
         startGame();
     }
 }
 
+// Update score, points, wins and losses
 function updateStatus() {
     $('#collect').html(game.randomScore);
     $('#yourScore').html(game.yourScore);
@@ -69,28 +78,36 @@ function updateStatus() {
     $('#losses').html(game.losses);
 }
 
+// When rupees are clicked
 $('.rupee').on('click', function(e) {
     let target = $(e.target);
     if (target.is('.green')) {
+        let sound = document.getElementById('pickGreen');
+        sound.play();
         game.yourScore += game.rupees.greenRupee;
         console.log('Added ' + game.rupees.greenRupee); // Delete later
     }
 
     if (target.is('.red')) {
+        let sound = document.getElementById('pickRed');
+        sound.play();
         game.yourScore += game.rupees.redRupee;
         console.log('Added ' + game.rupees.redRupee); // Delete later
     }
 
     if (target.is('.yellow')) {
+        let sound = document.getElementById('pickYellow');
+        sound.play();
         game.yourScore += game.rupees.yellowRupee;
         console.log('Added ' + game.rupees.yellowRupee); // Delete later
     }
 
     if (target.is('.silver')) {
+        let sound = document.getElementById('pickSilver');
+        sound.play();
         game.yourScore += game.rupees.silverRupee;
         console.log('Added ' + game.rupees.silverRupee); // Delete later
     }
-    console.log('Your score is ' + game.yourScore);
     scoreChecker();
     updateStatus();
 });
